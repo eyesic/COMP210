df2ea67fe63f37e4e58843a2ddfe7b5399cc066f
Isaac Tran <caasi@unc.edu>
2023-01-16 15:36:19 -0500

    COMP210 setup

--- src/assn01/HelloWorld.java
+++ src/assn01/HelloWorld.java
@@ -1,5 +1,6 @@
 package assn01;
 
-public class HelloWorld {
-    public static void main(String[] args) { System.out.println("Hello world!"); }
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");}
 }
